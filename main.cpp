5e3a932e82fe8cdf64e277544fd0395a43a09efc
OlgaMacko <kioto@inbox.lv>
2021-04-21 17:31:13 +0300

    Update main.cpp
    latv valodā

--- main.cpp
+++ main.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 int main() 
 {
-char otvet;
-cout << "Нажмите Y или N" <<endl;
-cout << "Ты живёшь в лесу?" <<endl;
-cin >> otvet;
-if (otvet=='Y') 
-  {cout << "Ты баба Яга?";
-  if (otvet=='Y') {cout<<"Что и детей пугаешь??";}
-  if (otvet=='N')  {cout << "Ты живёшь в пустыне?";
-          if (otvet=='Y') {cout << "Ты верблюд??";}
-          if (otvet=='N')  {cout << "Ты живёшь в городе?";}
+char atbilde;
+cout << "Nospiediet Y vai N" <<endl;
+cout << "Tu dzīvo mežā?" <<endl;
+cin >> atbilde;
+if (atbilde=='Y') 
+  {cout << "Tu esi ezis?";
+  if (otvet=='Y') {cout<<"Ņem abolu??";}
+  if (otvet=='N')  {cout << "Tu dzīvo tuksnesī?";
+          if (otvet=='Y') {cout << "Tu esi kamielis??";}
+          if (otvet=='N')  {cout << "Tu dzīvo pilsētā?";}
   }
   }
 return 0;
